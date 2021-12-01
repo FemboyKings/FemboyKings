b80539f38680bb58e7b032ea782df0bfe61e1b1c
FemboyKings <93692793+FemboyKings@users.noreply.github.com>
2021-12-01 14:12:35 +0100

    Update HourlyBot.py

--- HourlyBot.py
+++ HourlyBot.py
@@ -8,22 +8,19 @@ import time
 import os
 from random import choice
 
-image_urls = [
-    'https://drive.google.com/drive/folders/1q0UcKmWTBVLdzaYgrDf3VT3BUAPXFoTd?usp=sharing',
-    'https://drive.google.com/drive/folders/1q0UcKmWTBVLdzaYgrDf3VT3BUAPXFoTd?usp=sharing',
-    ]
 
 config = toml.load('config.toml')
-
 bot = telegram.Bot(config.get('token'), request = Request(con_pool_size = 70, connect_timeout = 120))
+
 
 run_async
 def hourly():
     while True:
         while datetime.datetime.utcnow().minute == 0 and datetime.datetime.utcnow().second == 0:
-            image_url = choice(image_urls)
-            bot.send_photo(config.get('channel'), image_url)
+            image = choice(os.listdir('./images/'))
+            bot.send_photo(config.get('channel'), open('./images/' + image, 'rb'))
         time.sleep(1)
+
 
 def main():
     updater = Updater(bot=bot, workers=66, use_context=True)
@@ -35,3 +32,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
