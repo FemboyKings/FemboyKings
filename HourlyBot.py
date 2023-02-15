71c9ab2cad5a6440d931e0d668c53ac182aa5588
FemboyKings <93692793+FemboyKings@users.noreply.github.com>
2023-02-15 23:52:57 +0100

    Update HourlyBot.py

--- HourlyBot.py
+++ HourlyBot.py
@@ -1,12 +1,10 @@
 import telegram
-from telegram.ext.dispatcher import run_async
-from telegram.utils.request import Request
-from telegram.ext import Updater, Dispatcher
+from telegram.ext import Updater
 import datetime
 import toml
 import time
 from random import choice
-
+from telegram.utils.request import Request
 
 config = toml.load('config.toml')
 bot = telegram.Bot(config.get('token'), request=Request(con_pool_size=70, connect_timeout=120))
@@ -6233,7 +6231,6 @@ image_urls = [
 
 
 
-run_async=True 
 def hourly():
     while True:
         if datetime.datetime.utcnow().minute == 0 and datetime.datetime.utcnow().second == 0:
@@ -6242,14 +6239,12 @@ def hourly():
             time.sleep(1)
         time.sleep(1)
 
-
 def main():
     updater = Updater(bot=bot, workers=66, use_context=True)
     updater.start_polling()
     print(f'Signed in as {bot.name}')
     hourly()
     updater.idle()
-
 
 if __name__ == '__main__':
     main()
